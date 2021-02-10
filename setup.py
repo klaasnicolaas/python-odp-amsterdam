23c68a89c377c2f7c4786c2dad2b11160b532395
Klaas Schoute <klaas_schoute@hotmail.com>
2021-02-10 14:50:46 +0100

    ⬆️ Update release number

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     name="garages_amsterdam",
     packages=find_packages(include=["garages_amsterdam"]),
     url="https://github.com/klaasnicolaas/garages_amsterdam",
-    version="2.0.0",
+    version="2.0.1",
     zip_safe=False,
 )
