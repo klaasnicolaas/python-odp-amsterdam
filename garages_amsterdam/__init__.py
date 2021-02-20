ad713212c336c627fd161e49ec235ba563c61965
Klaas Schoute <klaas_schoute@hotmail.com>
2021-02-20 04:01:54 +0100

    Revert "🔨 Change error handling (#6)" (#7)
    This reverts commit a04ef125668cdf20aa9f2c5fadff9806e7d3b435.

--- garages_amsterdam/__init__.py
+++ garages_amsterdam/__init__.py
@@ -1,13 +1,7 @@
 """Fetch latest parking garage information from Amsterdam."""
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientResponseError
 from dataclasses import dataclass
-from .import errors
-
 import logging
-import aiohttp
-import asyncio
-
-_LOGGER = logging.getLogger(__name__)
 
 @dataclass
 class AmsterdamCase:
@@ -57,22 +51,17 @@ def correct_name(name):
 
 async def get_garages(session: ClientSession, *, source=DEFAULT_SOURCE):
     """Fetch parking garage data."""
-    try:
-        resp = await session.get(source.URL)
-    except aiohttp.ClientConnectionError as err:
-        _LOGGER.debug("Failed to connect")
-        errors.raise_error(err, 1)
-    except aiohttp.InvalidURL as err:
-        _LOGGER.debug("Could not connect, API url is incorrect")
-        errors.raise_error(err, 1)
-    except aiohttp.ClientResponseError as err:
-        _LOGGER.debug("Caught response error: %s", err)
-        errors.raise_error(err, 2)
-    except (aiohttp.ClientError, asyncio.TimeoutError) as err:
-        _LOGGER.debug("Unknown error occurred")
-        errors.raise_error(err, 3)
-
+    resp = await session.get(source.URL)
     data = await resp.json(content_type=None)
+
+    if 'error' in data:
+        raise ClientResponseError(
+            resp.request_info,
+            resp.history,
+            status=data['error']['code'],
+            message=data['error']['message'],
+            headers=resp.headers
+        )
 
     results = []
     wrongKeys = ['FP','Fiets']
@@ -83,4 +72,5 @@ async def get_garages(session: ClientSession, *, source=DEFAULT_SOURCE):
                 results.append(source.from_json(item))
         except KeyError:
             logging.getLogger(__name__).warning("Got wrong data: %s", item)
+
     return results
