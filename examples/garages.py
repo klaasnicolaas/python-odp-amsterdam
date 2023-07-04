fb678a48b45e3685ac4a2b8f91adeaaea78f6309
Klaas Schoute <klaas_schoute@hotmail.com>
2023-07-04 17:12:09 +0200

    Change to list comprehension

--- examples/garages.py
+++ examples/garages.py
@@ -9,29 +9,24 @@ from odp_amsterdam import Garage, ODPAmsterdam
 async def main() -> None:
     """Show example on using the ODP Amsterdam API client."""
     async with ODPAmsterdam() as client:
-        count: int
-
         garages = await client.all_garages()
-        garage: Garage = await client.garage(
+        single_garage: Garage = await client.garage(
             garage_id="99b77fc5-a237-4ba0-abe4-b9a3886aa471",
         )
 
-        print(garage)
+        print(f"Single garage: {single_garage}")
         print()
 
-        for index, item in enumerate(garages, 1):
-            count = index
+        count: int = len(garages)
+        for item in garages:
             print(item)
 
-        # # Count unique id's in disabled_parkings
-        unique_values: list[str] = []
-        for location in garages:
-            unique_values.append(location.garage_id)
-        num_values = len(set(unique_values))
+        # Count unique id's in disabled_parkings
+        unique_values: list[str] = list({garage.garage_id for garage in garages})
 
         print("__________________________")
         print(f"Total garages found: {count}")
-        print(f"Unique ID values: {num_values}")
+        print(f"Unique ID values: {len(unique_values)}")
 
 
 if __name__ == "__main__":
--- examples/parking.py
+++ examples/parking.py
@@ -9,26 +9,21 @@ from odp_amsterdam import ODPAmsterdam
 async def main() -> None:
     """Show example on using the ODP Amsterdam API client."""
     async with ODPAmsterdam() as client:
-        count: int
-
         locations = await client.locations(
             limit=10,
             parking_type="E6a",
         )
 
-        for index, item in enumerate(locations, 1):
-            count = index
+        count: int = len(locations)
+        for item in locations:
             print(item)
 
         # Count unique id's in disabled_parkings
-        unique_values: list[str] = []
-        for location in locations:
-            unique_values.append(location.spot_id)
-        num_values = len(set(unique_values))
+        unique_values: list[str] = list({location.spot_id for location in locations})
 
         print("__________________________")
         print(f"Total locations found: {count}")
-        print(f"Unique ID values: {num_values}")
+        print(f"Unique ID values: {len(unique_values)}")
 
 
 if __name__ == "__main__":
